a24475ffce3ed7a7d2306ca9c50302a3801876dc
balaskandan <balaskandangiri@gmail.com>
2021-08-27 17:13:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react'
+import {BrowserRouter as Router, Link, Route, Switch, useRouteMatch, useHistory} from 'react-router-dom'
 import './App.css';
+import Products from './components/Products';
+import productService from './services/productService';
+import BusinessProfiles from './components/BusinessProfiles';
+import AddNewProfile from './components/AddNewProfile';
+import AddNewBusinessProfile from './components/AddNewBusinessProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        productService.getAll().then(initialProducts => {
+            console.log(initialProducts)
+            setProducts(initialProducts)
+        })
+    }, [loading])
+
+
+    const padding = {
+        paddingRight: 5
+      }
+    return (
+        <div class="container.fluid">
+            <h1>Craft</h1>
+            <div>
+                <Link to="/" style={padding}>Products</Link> &nbsp;&nbsp;
+                <Link to='/businessProfiles' style={padding}>Business profiles</Link>
+            </div>
+
+            <Switch>
+                <Route path="/createBusinessProfile">
+                    <AddNewBusinessProfile refresh={()=> {setLoading(!loading)}}></AddNewBusinessProfile>
+                </Route>
+                <Route path="/createProduct">
+                    <AddNewProfile refresh={()=> {setLoading(!loading)}}></AddNewProfile>
+                </Route> 
+                <Route path="/businessProfiles">
+                    <BusinessProfiles refresh={()=> {setLoading(!loading)}}/>
+                </Route> 
+                <Route path="/">
+                    <Products products={products} refresh={()=> {setLoading(!loading)}}></Products>
+                </Route>
+            </Switch>
+
+            {/* <Footer></Footer> */}
+        </div>
+    );
 }
 
 export default App;
