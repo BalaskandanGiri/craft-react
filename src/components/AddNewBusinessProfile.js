0203eaf2c13446b95850ac9586f8e0cc31ab9d72
balaskandan <balaskandangiri@gmail.com>
2021-08-27 17:13:24 +0530

    Business profile

---
+++ src/components/AddNewBusinessProfile.js
@@ -0,0 +1,54 @@
+
+import React, { useState } from 'react'
+import {BrowserRouter as Router, Link, Route, Switch, useRouteMatch, useHistory} from 'react-router-dom'
+import businessProfilesService from '../services/businessProfilesService'
+import productService from '../services/productService'
+
+const AddNewBusinessProfile = ({refresh}) => {
+    const [companyName, setCompanyName] = useState('')
+    const [legalName, setLegalName] = useState('')
+    const [email, setEmail] = useState('')
+    const [website, setWebsite] = useState('')
+    const [taxIdentifier, setTaxIdentifier] = useState('')
+    const [products, setProducts] = useState([])
+    const [businessAddress, setBusinessAddress] = useState({building: '', street: '', city: '', state: '', country: '', pincode: ''})
+    const history = useHistory()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        businessProfilesService.create({companyName, legalName, email, website, taxIdentifier, businessAddress, products}).then(() => { 
+            refresh();
+            history.push("/businessProfiles");
+        })
+    }
+
+    return(
+        <div>
+            <h2>Add a new</h2>
+            <form onSubmit={submitHandler}>
+                <table>
+                    <tbody>
+                        <tr><td>companyName:</td><td><input value={companyName} onChange={(e) => {setCompanyName(e.target.value)}}/></td></tr>
+                        <tr><td>legalName:</td><td><input value={legalName} onChange={(e) => {setLegalName(e.target.value)}}/></td></tr>
+                        <tr><td>email:</td><td><input value={email} onChange={(e) => {setEmail(e.target.value)}}/></td></tr>
+                        <tr><td>website:</td><td><input value={website} onChange={(e) => {setWebsite(e.target.value)}}/></td></tr>
+                        <tr><td>taxIdentifier:</td><td><input value={taxIdentifier} onChange={(e) => {setTaxIdentifier(e.target.value)}}/></td></tr>
+                        <tr><td>building:</td><td><input value={businessAddress.building} onChange={(e) => {setBusinessAddress({...businessAddress, building: e.target.value})}}/></td></tr>
+                        <tr><td>street:</td><td><input value={businessAddress.street} onChange={(e) => {setBusinessAddress({...businessAddress, street: e.target.value})}}/></td></tr>
+                        <tr><td>city:</td><td><input value={businessAddress.city} onChange={(e) => {setBusinessAddress({...businessAddress, city: e.target.value})}}/></td></tr>
+                        <tr><td>state:</td><td><input value={businessAddress.state} onChange={(e) => {setBusinessAddress({...businessAddress, state: e.target.value})}}/></td></tr>
+                        <tr><td>country:</td><td><input value={businessAddress.country} onChange={(e) => {setBusinessAddress({...businessAddress, country: e.target.value})}}/></td></tr>
+                        <tr><td>pincode:</td><td><input value={businessAddress.coupincodentry} onChange={(e) => {setBusinessAddress({...businessAddress, pincode: e.target.value})}}/></td></tr>
+                        <tr><td>products:</td><td><input value={products.join(',')} onChange={(e) => {setProducts(e.target.value.split(","))}}/></td></tr>
+                        <tr><td></td><td><button type="submit" class="btn btn-primary">add</button></td></tr>
+                    </tbody>
+                </table>
+
+
+            </form>
+        </div>
+    );
+
+}
+
+export default AddNewBusinessProfile;
---
+++ src/components/BusinessProfiles.js
@@ -0,0 +1,57 @@
+
+import React, { useState, useEffect } from 'react'
+import {BrowserRouter as Router, Link, Route, Switch, useRouteMatch, useHistory} from 'react-router-dom'
+import businessProfilesService from '../services/businessProfilesService';
+
+
+
+const BusinessProfiles = ({refresh}) => {
+    const [profiles, setProfiles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        businessProfilesService.getAll().then(data => {
+            console.log(data)
+            setProfiles(data)
+        })
+    }, [loading])
+
+    if(!profiles || profiles.length === 0) return null
+
+    const convertAddressToStr = (address) => {
+        return address.building + ', ' + address.street + ', ' + address.city + ', ' + address.state + ', ' + address.country;
+    }
+
+    const getProductNames = (products) => {
+        return products.map(product => product.name).join(', ');
+    }
+
+    const deleteBusinessProfile = (businessProfile) => {
+        return <button type="button" class="btn btn-danger" onClick={() => {businessProfilesService.deleteItem(businessProfile.id).then(() => {setLoading(!loading)});}}>delete</button>
+    }
+    return (
+        <div>
+            <center><h2>BusinessProfiles</h2></center>
+            <br/>
+            <Link to="/createBusinessProfile"><button type="button" class="btn btn-primary">Create</button></Link><br/>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">companyName</th>
+                    <th scope="col">legalName</th>
+                    <th scope="col">email</th>
+                    <th scope="col">website</th>
+                    <th scope="col">taxIdentifier</th>
+                    <th scope="col">businessAddress</th>
+                    <th scope="col">products</th>
+                    <th scope="col">delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {profiles.map(profile => (<tr><td>{profile.companyName}</td><td>{profile.legalName}</td><td>{profile.email}</td><td>{profile.website}</td><td>{profile.taxIdentifier}</td><td>{convertAddressToStr(profile.businessAddress)}</td><td>{getProductNames(profile.products)}</td><td>{deleteBusinessProfile(profile)}</td></tr>))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default BusinessProfiles;
